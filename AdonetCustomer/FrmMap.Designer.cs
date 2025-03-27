6e24cc93e6fb3652e9e0252cf13b95ec5b4f3d62
Kadir Kobak <kadir.kobak@celsan.com>
2025-03-27 09:44:31 +0300

    Forms updated.

---
+++ AdonetCustomer/FrmMap.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace AdonetCustomer
+{
+    partial class FrmMap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenCityForm = new System.Windows.Forms.Button();
+            this.btnOpenCustomerForm = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenCityForm
+            // 
+            this.btnOpenCityForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOpenCityForm.Location = new System.Drawing.Point(68, 36);
+            this.btnOpenCityForm.Name = "btnOpenCityForm";
+            this.btnOpenCityForm.Size = new System.Drawing.Size(318, 69);
+            this.btnOpenCityForm.TabIndex = 0;
+            this.btnOpenCityForm.Text = "City Transaction";
+            this.btnOpenCityForm.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenCustomerForm
+            // 
+            this.btnOpenCustomerForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOpenCustomerForm.Location = new System.Drawing.Point(68, 120);
+            this.btnOpenCustomerForm.Name = "btnOpenCustomerForm";
+            this.btnOpenCustomerForm.Size = new System.Drawing.Size(318, 69);
+            this.btnOpenCustomerForm.TabIndex = 1;
+            this.btnOpenCustomerForm.Text = "Customer Transaction";
+            this.btnOpenCustomerForm.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnExit.Location = new System.Drawing.Point(68, 208);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(318, 69);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // FrmMap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 307);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOpenCustomerForm);
+            this.Controls.Add(this.btnOpenCityForm);
+            this.Name = "FrmMap";
+            this.Text = "Forms";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenCityForm;
+        private System.Windows.Forms.Button btnOpenCustomerForm;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
